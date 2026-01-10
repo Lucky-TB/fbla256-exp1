55ae8dfcc2ac501f168a108217c96c2305bafb26
luckytb <90356849+Lucky-TB@users.noreply.github.com>
2026-01-10 12:46:45 -0500

    Created navbar

---
+++ app/(tabs)/resources.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const resources = () => {
+  return (
+    <View>
+      <Text>resources</Text>
+    </View>
+  )
+}
+
+export default resources
